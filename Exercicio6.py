bfb45ea63911ae4bd37662c0e191420a7340e290
Guna-ME <Dudamarcos151@hotmail.com>
2020-09-16 12:08:37 -0300

    Feat: adiciona exercicio 6

---
+++ Exercicio6.py
@@ -0,0 +1,13 @@
+listaFilmes = ["Cidade dos Ossos","Little Mermaid","Pets"]
+print("Filmes: ", listaFilmes)
+listaJogos = ["LOL","Final Fantasy","Minecraft"]
+print("Jogos: ", listaJogos)
+listaLivros = ["Corte de Rosas e Espinhos","Trono de Vidro","Hush Hush"]
+print("Livros: ", listaLivros)
+listaEsportes = ["Handebol","Futebol","Volei"]
+print("EEsportes: ",listaEsportes)
+
+listaConjunta = listaFilmes + listaJogos + listaLivros + listaEsportes
+print("Itens de todas as listas: ", listaConjunta)
+
+print("Meu livro favorito: ", listaLivros[0])
