abaf96247ecf9ce251b83c030ac139afe4b2c175
Guna-ME <Dudamarcos151@hotmail.com>
2020-09-16 12:09:08 -0300

    Feat: adiciona exercicio 7

---
+++ Exercicio7.py
@@ -0,0 +1,6 @@
+itensGamer = ["cooler master","GeForce RTX 2080Ti","Gabinete Gamer Mid Tower BLACK HAWK RGB FORTREK","Headset gamer pro kp-416","Mouse gamer kp-23","Teclado gamer pro kp-2041"]
+print("Meus componentes Gamer: ", itensGamer)
+itensGamer.remove("GeForce RTX 2080Ti")
+print("Meus componentes após a remoção: ", itensGamer)
+itensGamer.remove("cooler master")
+print("Meus componentes após a remoção: ", itensGamer)
